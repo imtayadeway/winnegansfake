c6eb3c6450c7eea9482455fe17379399957f3172
Tim Wade <hello@timjwade.com>
2025-11-20 18:21:07 -0800

    hmmmmm

--- lib/winnegansfake/chunk.rb
+++ lib/winnegansfake/chunk.rb
@@ -14,12 +14,25 @@ module WinnegansFake
       @text ||=
         begin
           file.pos = cursor.get
-          t = file.readpartial(size).strip
+          t = file.readpartial(size)
           if file.eof?
             file.rewind
             t.concat(" " + file.readpartial(size - t.size))
           end
-          t
+
+          unless t.end_with?(" ")
+            t.sub!(/[^ ]+\z/, "")
+          end
+
+          if t.match?(/\n/)
+            t.sub!(/\n.*\z/, "")
+          end
+
+          if t[(size * 0.75).floor..-1].match?(/[,\.]/)
+            t.sub!(/[^,\.]+\z/, "")
+          end
+
+          t.strip.chomp
         end
     end
 
--- test/winnegansfake/chunk_test.rb
+++ test/winnegansfake/chunk_test.rb
@@ -5,7 +5,7 @@ class ChunkTest < Minitest::Test
   def test_text
     file = StringIO.new("The quick brown fox jumps over the lazy dog")
     cursor = Struct.new(:get).new(0)
-    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 19)
+    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 20)
 
     chunk = chunker.text
 
@@ -15,7 +15,7 @@ class ChunkTest < Minitest::Test
   def test_reads_from_cursor
     file = StringIO.new("The quick brown fox jumps over the lazy dog")
     cursor = Struct.new(:get).new(4)
-    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 15)
+    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 16)
 
     chunk = chunker.text
 
@@ -35,7 +35,7 @@ class ChunkTest < Minitest::Test
   def test_next_pos
     file = StringIO.new("The quick brown fox jumps over the lazy dog")
     cursor = Struct.new(:get).new(0)
-    chunk = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 19)
+    chunk = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 20)
 
     assert_equal "The quick brown fox", chunk.text
 
@@ -48,10 +48,40 @@ class ChunkTest < Minitest::Test
   def test_wraps_around
     file = StringIO.new("the quick brown fox jumps over the lazy dog")
     cursor = Struct.new(:get).new(30)
-    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 31)
+    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 33)
 
     chunk = chunker.text
 
     assert_equal "the lazy dog the quick brown fox", chunk
+  end
+
+  def test_whole_words
+    file = StringIO.new("The quick brown fox jumps over the lazy dog")
+    cursor = Struct.new(:get).new(0)
+    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 22)
+
+    chunk = chunker.text
+
+    assert_equal "The quick brown fox", chunk
+  end
+
+  def test_newline
+    file = StringIO.new("The quick brown fox\njumps over the lazy dog")
+    cursor = Struct.new(:get).new(0)
+    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 26)
+
+    chunk = chunker.text
+
+    assert_equal "The quick brown fox", chunk
+  end
+
+  def test_punctuation
+    file = StringIO.new("The quick brown fox jumps over the lazy dog. The quick brown fox...")
+    cursor = Struct.new(:get).new(0)
+    chunker = WinnegansFake::Chunk.new(file: file, cursor: cursor, size: 58)
+
+    chunk = chunker.text
+
+    assert_equal "The quick brown fox jumps over the lazy dog.", chunk
   end
 end
