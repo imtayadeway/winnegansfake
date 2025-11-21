c4564c136e3211176f7418b7d324fe39f8aa2d7c
Tim Wade <hello@timjwade.com>
2025-11-20 18:45:23 -0800

    extract get_text

--- lib/winnegansfake/chunk.rb
+++ lib/winnegansfake/chunk.rb
@@ -11,37 +11,42 @@ module WinnegansFake
     end
 
     def text
-      @text ||=
-        begin
-          file.pos = cursor.get
-          t = file.readpartial(size)
-          if file.eof?
-            file.rewind
-            t.concat(" " + file.readpartial(size - t.size))
-          end
-
-          #  maybe try to peek instead
-          unless file.pread(1, file.pos).match?(/\s/)
-            t.sub!(/[^\s]+\z/, "")
-          end
-
-          # linebreaks
-          if t.match?(/\n/)
-            t.sub!(/\n.*\z/, "")
-          end
-
-          # wraparound
-          if t[(size * 0.75).floor..-1].match?(/[,\.]/)
-            t.sub!(/[^,\.]+\z/, "")
-          end
-
-          t.strip.chomp
-        end
+      @text ||= get_text
     end
 
     def next_pos
       text
       file.pos
+    end
+
+    private
+
+    def get_text
+      file.pos = cursor.get
+      t = file.readpartial(size)
+
+      # wraparound
+      if file.eof?
+        file.rewind
+        t.concat(" " + file.readpartial(size - t.size))
+      end
+
+      # end on whole word
+      unless file.pread(1, file.pos).match?(/\s/)
+        t.sub!(/[^\s]+\z/, "")
+      end
+
+      # linebreaks
+      if t.match?(/\n/)
+        t.sub!(/\n.*\z/, "")
+      end
+
+      # end near punctuation
+      if t[(size * 0.75).floor..-1].match?(/[,\.]/)
+        t.sub!(/[^,\.]+\z/, "")
+      end
+
+      t.strip.chomp
     end
   end
 end
