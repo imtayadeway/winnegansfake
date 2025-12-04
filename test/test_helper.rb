a527c2d9b5672d8391ac601e6070a1345d0bfd0d
Tim Wade <hello@timjwade.com>
2025-12-03 17:33:19 -0800

    Don't need this anymore

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,19 +17,3 @@ module WinnegansFake
     end
   end
 end
-
-if WinnegansFake::DB.table_exists?(:cursor)
-  WinnegansFake::DB.drop_table :cursor
-end
-WinnegansFake::DB.create_table :cursor do
-  primary_key :id
-  Integer :value, null: false
-end
-
-if WinnegansFake::DB.table_exists?(:timestamp)
-  WinnegansFake::DB.drop_table :timestamp
-end
-WinnegansFake::DB.create_table :timestamp do
-  primary_key :id
-  DateTime :value, null: false
-end
